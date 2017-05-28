eacbcb1c926480487f2d1be7957cd178a6a4115b
Tommy Parnell <tparnell8@gmail.com>
2017-05-27 19:07:43 -0700

    add delete job, output to output

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 ﻿var p = require('./package.json');
 var gulp = require('gulp');
+var path = require('path');
+var del = require('del');
 var configuration = process.env.BUILD_CONFIGURATION || 'Release';
 var {restore, build, test, pack, push} = require('gulp-dotnet-cli');
 
 gulp.task('default', ['nuget']);
 
-gulp.task('restore', [], ()=>{
+gulp.task('clean', [], ()=>del(['output', 'src/**/bin', 'src/**/obj']))
+
+gulp.task('restore', ['clean'], ()=>{
 
     return gulp.src('**/*.sln')
                .pipe(restore());
@@ -36,14 +40,15 @@ gulp.task('nuget', ['build'], ()=>{
     return gulp.src('src/Otp.NET/Otp.NET.csproj')
                .pipe(pack({
                    version: p.version,
-                   configuration: configuration
+                   configuration: configuration,
+                   output: path.resolve(__dirname, 'output')
                }));
 
 }, {read:false});
 
 gulp.task('push', ['nuget'], ()=>{
 
-    return gulp.src('**/*.nupkg')
+    return gulp.src('output/**/*.nupkg')
                .pipe(push({
                    apiKey: process.env.NUGET_API_KEY
                }));
